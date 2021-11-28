280af97cc70f579656731dfa86b9e48ec9a2ab16
Axel-Makes-it <95051495+Axel-Makes-it@users.noreply.github.com>
2021-11-27 23:23:52 -0500

    added add to basket and checkout

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD-LvVPBcJKINKFkj5tzp3wO10EmydUUWU",
+    authDomain: "clone-23c9a.firebaseapp.com",
+    projectId: "clone-23c9a",
+    storageBucket: "clone-23c9a.appspot.com",
+    messagingSenderId: "1043654825721",
+    appId: "1:1043654825721:web:1bb061c875686a0b167428"
+  };
